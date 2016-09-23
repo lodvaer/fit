c63e88cd1229830bcfad02612f6f7cf24bfaf48d
Lodvær <lodvaer@gmail.com>
2016-09-23 15:59:06 +0000

    2016-09-23

--- calc.py
+++ calc.py
@@ -3,6 +3,8 @@
 import yaml
 import os
 
+BMR = 2090
+
 PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), "food.yml")
 
 with open(PATH) as f:
@@ -21,10 +23,34 @@ def food_sum(food, kcal = 0, p = 0):
             p += food['p']*qty if 'p' in food else 0
     return kcal, p
 
+# Calculate burn using BMR and Harris-Benedict exercise multipliers.
+def burn(food):
+    def mul():
+        if 'activity' not in food[0]:
+            return 1.2
+        act = food[0]['activity']
+        if act == 0:
+            return 1.2
+        if act == 1:
+            return 1.375
+        if act == 2:
+            return 1.55
+        if act == 3:
+            return 1.725
+        if act == 4:
+            return 1.9
+        raise Exception(str(act)+"? No you didn't.")
+
+    return BMR*mul()
+
 def main():
+    kg = 0
     for day, food in sorted(YML['days'].items()):
         kcal, p = food_sum(food)
-        print(day.isoformat(), '\tkcal:', kcal, '\tp:', p)
+        diff = kcal - burn(food)
+        kg += diff/7700
+        fmt = "{:%Y-%m-%d}\tkcal:{}\tp:{}\tdiff:{}\tkg:{:.1}"
+        print(fmt.format(day, kcal, p, diff, kg))
 
 if __name__ == '__main__':
     main()
